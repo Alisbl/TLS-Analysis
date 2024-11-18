553ef92d8435d689511bf88bff5fb5d1448181bc
Alisbl <121486404+Alisbl@users.noreply.github.com>
2024-11-18 15:55:46 +0200

    Update fill_client_service_column.py

--- Function.py/fill_client_service_column.py
+++ Function.py/fill_client_service_column.py
@@ -1,5 +1,5 @@
+import pandas as pd
 def fill_client_service_column(client_data):
-    import pandas as pd
     """
     Fill the 'Service' column in the Client Hello data.
     This function uses the 'SNI' column to assign a service.
